f042d9b4eacc0f65c8ab153f7cd736c998619acb
Rokon Ron <rokon.raz@gmail.com>
2023-05-27 00:17:06 +0600

    It can also be written like this.using with keyword and sit variable connection.

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import smtplib
 email = "roko.ron017@gmail.com"
 password = "Rokoron559445689"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=email,password=password)
-connection.sendmail(
-    from_addr=email,
-    to_addrs="rokon.raz@gmail.com",
-    msg="Subject:Hello\n\nThis is the body of the mail"
-)
-
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=email,password=password)
+    connection.sendmail(
+        from_addr=email,
+        to_addrs="rokon.raz@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of the mail"
+    )
